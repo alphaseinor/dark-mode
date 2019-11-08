88e4cf4bee0fc2553f44c7dedfdde5c9a0c02101
Brian Hague <alphaseinor@gmail.com>
2019-11-07 20:19:51 -0600

    fixed localstorage bug, setValue wasn't being returned

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -2,8 +2,10 @@ import {useState} from 'react'
 
 export const useLocalStorage = (key, initialValue) =>{
   const [storedValue, setStoredValue] = useState(() => {
+
     const item = window.localStorage.getItem(key)
     return item ? JSON.parse(item) : initialValue
+  
   });
 
   //setter
@@ -12,5 +14,5 @@ export const useLocalStorage = (key, initialValue) =>{
     window.localStorage.setItem(key, JSON.stringify(value))
   }
 
-  return [storedValue]
+  return [storedValue, setValue]
 }
